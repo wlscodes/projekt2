936ccc2c17561339283595422e15ff6b6deee9ca
wlscodes <49492132+wlscodes@users.noreply.github.com>
2019-05-15 18:08:16 +0200

    Add files via upload

---
+++ PrimeNumber.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt2
+{
+    class PrimeNumber
+    {
+        public long counter;
+
+        public bool IsPrimeDefaultWithCounter(BigInteger number)
+        {
+            counter = 1;
+            if (number < 2) return false;
+            else if (number < 4) return true;
+            else if (number % 2 == 0) return false;
+            else
+            {
+                for (BigInteger i = 3; i < number / 2; i += 2)
+                {
+                    counter++;
+                    if (number % i == 0) return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsPrimeDefault(BigInteger number)
+        {
+            if (number < 2) return false;
+            else if (number < 4) return true;
+            else if (number % 2 == 0) return false;
+            else
+            {
+                for (BigInteger i = 3; i < number / 2; i += 2)
+                {
+                    if (number % i == 0) return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsPrimeDecentWithCounter(BigInteger number)
+        {
+            counter = 1;
+            if (number < 2) return false;
+            else if (number < 4) return true;
+            else if (number % 2 == 0) return false;
+            else
+            {
+                for (BigInteger i = 3; i * i <= number; i += 2)
+                {
+                    counter++;
+                    if (number % i == 0) return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsPrimeDecent(BigInteger number)
+        {
+            if (number < 2) return false;
+            else if (number < 4) return true;
+            else if (number % 2 == 0) return false;
+            else
+            {
+                for (BigInteger i = 3; i * i <= number; i += 2)
+                {
+                    if (number % i == 0) return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrimeEngine();
+
+            Console.WriteLine(Stopwatch.Frequency);
+
+            Console.ReadKey();
+        }
+
+        static void PrimeEngine()
+        {
+            PrimeNumber primeNumber = new PrimeNumber();
+
+            //BigInteger[] numberArray = {101, 1009, 10091, 100913, 1009139, 10091401};
+            BigInteger[] numberArraySmall = { 101, 1009, 100913, 1009139, 10091401, 100914061, 1009140611, 10091406133 };
+            BigInteger[] numberArray = { 101, 1009, 100913, 1009139, 10091401, 100914061, 1009140611, 10091406133, 100914061337, 1009140613399 };
+
+            EngineForDefaultWithCounter(primeNumber, numberArraySmall);
+
+            EngineForDecentWithCounter(primeNumber, numberArray);
+
+            EngineForDefaultWithTimer(primeNumber, numberArraySmall);
+
+            EngineForDecentWithTimer(primeNumber, numberArray);
+        }
+
+        #region counters
+        static void EngineForDefaultWithCounter(PrimeNumber pn, BigInteger[] arr)
+        {
+            Console.WriteLine("Default - Counter");
+            Console.WriteLine("Number;IsPrime;Counter");
+            foreach(BigInteger b in arr)
+            {
+                Console.WriteLine("{0};{1};{2}", b, pn.IsPrimeDefaultWithCounter(b), pn.counter);
+            }
+        }
+
+        static void EngineForDecentWithCounter(PrimeNumber pn, BigInteger[] arr)
+        {
+            Console.WriteLine("Decent - Counter");
+            Console.WriteLine("Number;IsPrime;Counter");
+            foreach (BigInteger b in arr)
+            {
+                Console.WriteLine("{0};{1};{2}", b, pn.IsPrimeDecentWithCounter(b), pn.counter);
+            }
+        }
+        #endregion
+
+        #region timers
+        static void EngineForDefaultWithTimer(PrimeNumber pn, BigInteger[] arr)
+        {
+            Console.WriteLine("Default - Time");
+            Console.WriteLine("Number;IsPrime;Time");
+            foreach (BigInteger b in arr)
+            {
+                long start = Stopwatch.GetTimestamp();
+                bool isPrime = pn.IsPrimeDefault(b);
+                long stop = Stopwatch.GetTimestamp();
+                Console.WriteLine("{0};{1};{2}", b, isPrime, stop - start);
+            }
+        }
+
+        static void EngineForDecentWithTimer(PrimeNumber pn, BigInteger[] arr)
+        {
+            Console.WriteLine("Decent - Time");
+            Console.WriteLine("Number;IsPrime;Time");
+            foreach (BigInteger b in arr)
+            {
+                long start = Stopwatch.GetTimestamp();
+                bool isPrime = pn.IsPrimeDecent(b);
+                long stop = Stopwatch.GetTimestamp();
+                Console.WriteLine("{0};{1};{2}", b, isPrime, stop - start);
+            }
+        }
+        #endregion
+    }
+}
